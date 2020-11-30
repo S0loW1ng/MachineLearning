76bd22f756241200b7e7253ef356fb14998473d4
eegala <72153348+eegala@users.noreply.github.com>
2020-11-29 18:53:42 -0800

    Finalized code
    Almost ready for delivery.

--- ProjectCode/418_Classifier_Flat_NonFlat_PCA_Gradient_V90.py
+++ ProjectCode/418_Classifier_Flat_NonFlat_PCA_Gradient_V90.py
@@ -91,18 +91,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_
 # PCA uses all of the data points and relates them to get the basis vectors(c1,c2,...,cn), we have 960 images, and...
 # 4096 pixels, (diff dimensions)
 
-# You are supposed to do to the test data exactly the same transformation that you did to the training data
-# (multiply train set PCs to test set; this includes centering -- it should be done using the mean values
-# obtained on the training set. If you standardized the training set, then you would also divide your test set
-# by the standard deviations obtained on the training set.
-# After that, you can project your test set onto the PCs of the training set.
-# Can also be found on Lecture 10 slide 13?, should've seen that before...
-# https://stats.stackexchange.com/questions/142216/zero-centering-the-testing-set-after-pca-on-the-training-set
-# https://stats.stackexchange.com/questions/55718/pca-and-the-train-test-split
-# https://datascience.stackexchange.com/questions/45900/when-to-use-standard-scaler-and-when-normalizer
-# X_std_train = StandardScaler().fit_transform(X_train)
-# X_nml_train = Normalizer().fit_transform(X_train)
-
 #   3.1 Preparing dataframe for PCA. Using training dataframe for PCA
 #       Mean Centering, and normalizing training dataframe.
 meanTrainSet = X_train.mean(axis=0)  # Axis 0 is column means in 2D matrix, which is what we want
@@ -112,7 +100,8 @@ X_Centered_Train = (X_train - meanTrainSet) / StdTrainSet
 # Performing PCA onto training dataframe, from 4096 components to 200.
 pca = PCA(n_components=200)
 X_pca_train = pca.fit_transform(X_Centered_Train)
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
+Cumulative_Var_Ratio = np.cumsum(pca.explained_variance_ratio_)
+plt.plot(Cumulative_Var_Ratio)
 plt.title('Cumulative Sum of Variance Ratio')
 plt.xlabel('Number of Components')
 plt.ylabel('Variance Ratio')
@@ -120,6 +109,31 @@ plt.show()
 
 Xtest_centered = (X_test - meanTrainSet) / StdTrainSet
 X_pca_test_centered = pca.transform(Xtest_centered)  # PCA extracted from training set applied to test set
+
+# Generating plot for three principal components and their respective label (0 or 1)
+fig = plt.figure()
+ax = Axes3D(fig)
+plt.title('3 PCA Components')
+ax.scatter(X_pca_train[:, 0], X_pca_train[:, 1], X_pca_train[:, 2], c=y_train, alpha=0.8,
+           cmap=plt.cm.get_cmap('Spectral', 10))
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+ax.set_zlabel('Component 3')
+plt.show()
+
+# Recovering 64x64 image after going through PCA and reducing 4096 dimensions to 200 (Just for comparison purposes)
+X_recovered = pca.inverse_transform(X_pca_test_centered)
+X_test_np = X_test.values
+
+f = plt.figure()
+f.add_subplot(1, 2, 1)
+plt.title("Original")
+plt.imshow(X_test_np[3].reshape((64, 64)), cmap='gray')
+f.add_subplot(1, 2, 2)
+plt.title("PCA compressed")
+plt.imshow(X_recovered[3].reshape((64, 64)), cmap='gray')
+plt.show(block=True)
+
 
 # 4 Classifier (MLP) (Parameters set by whole team discussion)
 
@@ -132,33 +146,8 @@ ypred = model.predict(X_pca_test_centered)
 print(metrics.classification_report(ypred, y_test))
 mat = confusion_matrix(y_test, ypred)
 sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False)
+plt.title('Confusion Matrix for Model Testing')
 plt.xlabel('True Label')
 plt.ylabel('Predicted Label')
 plt.show()
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.scatter(X_pca_train[:, 0], X_pca_train[:, 1], X_pca_train[:, 2], c=y_train, alpha=0.8,
-           cmap=plt.cm.get_cmap('Spectral', 10))
-plt.xlabel('component 1')
-plt.ylabel('component 2')
-ax.set_zlabel('component 3')
-plt.show()
-
-plt.scatter(X_Centered_Train[0], X_Centered_Train[1], c=y_train)
-plt.xlim(-1, 10)
-plt.ylim(-1, 10)
-plt.show()
-
-X_recovered = pca.inverse_transform(X_pca_test_centered)
-X_test_np = X_test.values
-
-f = plt.figure()
-f.add_subplot(1, 2, 1)
-plt.title("original")
-plt.imshow(X_test_np[3].reshape((64, 64)), cmap='gray')
-f.add_subplot(1, 2, 2)
-
-plt.title("PCA compressed")
-plt.imshow(X_recovered[3].reshape((64, 64)), cmap='gray')
-plt.show(block=True)
 
